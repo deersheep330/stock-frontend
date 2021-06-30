d72045f4295ee75bdf216081af6dd4e765d92b71
Yang Chiu <deersheep330@gmail.com>
2021-06-30 20:21:14 +0800

    test more charts

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ Highcharts.theme = {
       "fontFamily": "Inconsolata",
       "color": "#A2A39C"
     },
-    "marginBottom": 100
+    "height": "50%"
   },
   "title": {
     "style": {
@@ -26,9 +26,7 @@ Highcharts.theme = {
     "itemStyle": {
       "fontWeight": "normal",
       "color": "#A2A39C"
-    },
-    "x": 0,
-    "y": 0
+    }
   },
   "xAxis": {
     "gridLineDashStyle": "Dot",
@@ -55,7 +53,7 @@ Highcharts.theme = {
 Highcharts.setOptions(Highcharts.theme);
 
 
-Highcharts.chart('container', {
+Highcharts.chart('container-1', {
 
   title: {
     text: 'Solar Employment Growth by Sector, 2010-2016'
@@ -64,12 +62,6 @@ Highcharts.chart('container', {
   yAxis: {
     title: {
       text: 'Number of Employees'
-    }
-  },
-
-  xAxis: {
-    accessibility: {
-      rangeDescription: 'Range: 2010 to 2017'
     }
   },
 
@@ -99,19 +91,124 @@ Highcharts.chart('container', {
     data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
   }],
 
-  responsive: {
-    rules: [{
-      condition: {
-        maxWidth: 500
+});
+
+Highcharts.chart('container-2', {
+
+  title: {
+    text: 'Solar Employment Growth by Sector, 2010-2016'
+  },
+
+  yAxis: {
+    title: {
+      text: 'Number of Employees'
+    }
+  },
+
+  plotOptions: {
+    series: {
+      label: {
+        connectorAllowed: false
       },
-      chartOptions: {
-        legend: {
-          layout: 'horizontal',
-          align: 'center',
-          verticalAlign: 'bottom'
-        }
-      }
-    }]
-  }
+      pointStart: 2010
+    }
+  },
+
+  series: [{
+    name: 'Installation',
+    data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+  }, {
+    name: 'Manufacturing',
+    data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+  }, {
+    name: 'Sales & Distribution',
+    data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+  }, {
+    name: 'Project Development',
+    data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+  }, {
+    name: 'Other',
+    data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+  }],
+
+});
+
+Highcharts.chart('container-3', {
+
+  title: {
+    text: 'Solar Employment Growth by Sector, 2010-2016'
+  },
+
+  yAxis: {
+    title: {
+      text: 'Number of Employees'
+    }
+  },
+
+  plotOptions: {
+    series: {
+      label: {
+        connectorAllowed: false
+      },
+      pointStart: 2010
+    }
+  },
+
+  series: [{
+    name: 'Installation',
+    data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+  }, {
+    name: 'Manufacturing',
+    data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+  }, {
+    name: 'Sales & Distribution',
+    data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+  }, {
+    name: 'Project Development',
+    data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+  }, {
+    name: 'Other',
+    data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+  }],
+
+});
+
+Highcharts.chart('container-4', {
+
+  title: {
+    text: 'Solar Employment Growth by Sector, 2010-2016'
+  },
+
+  yAxis: {
+    title: {
+      text: 'Number of Employees'
+    }
+  },
+
+  plotOptions: {
+    series: {
+      label: {
+        connectorAllowed: false
+      },
+      pointStart: 2010
+    }
+  },
+
+  series: [{
+    name: 'Installation',
+    data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+  }, {
+    name: 'Manufacturing',
+    data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+  }, {
+    name: 'Sales & Distribution',
+    data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+  }, {
+    name: 'Project Development',
+    data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+  }, {
+    name: 'Other',
+    data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+  }],
 
 });
